bd516370ffbf64491505f5f378c28cd7bba21a2d
Jon Benson <bensojon@gmail.com>
2016-01-12 13:26:44 -0800

    Update artifact module output

--- terraform/modules/aws/util/artifact/artifact.tf
+++ terraform/modules/aws/util/artifact/artifact.tf
@@ -8,7 +8,7 @@ variable "atlas_username"    { }
 variable "artifact_name"     { }
 variable "artifact_versions" { default = "latest" }
 
-resource "atlas_artifact" "mod" {
+resource "atlas_artifact" "artifact" {
   name    = "${var.atlas_username}/${var.artifact_name}"
   type    = "${var.type}"
   count   = "${length(split(",", var.artifact_versions))}"
@@ -21,4 +21,4 @@ resource "atlas_artifact" "mod" {
   }
 }
 
-output "amis" { value = "${atlas_artifact.mod.metadata_full.*.ami_id}" }
+output "amis" { value = "${join(",", atlas_artifact.artifact.*.metadata_full.ami_id)}" }
