43b0ba1c252b8d7876ae7f841238ee9dc7cfc923
Cameron Stokes <cameron@cameronstokes.com>
2016-01-31 20:04:49 -0800

    One NAT gateway per public subnet (left commented)

--- terraform/modules/aws/network/nat/nat.tf
+++ terraform/modules/aws/network/nat/nat.tf
@@ -3,21 +3,24 @@
 #--------------------------------------------------------------
 
 variable "name"              { default = "nat" }
+variable "azs"               { }
 variable "public_subnet_ids" { }
 
 resource "aws_eip" "nat" {
-  # count = "${length(split(",", var.public_subnet_ids))}" # Comment out count to only have 1 NAT
   vpc   = true
+
+  #count = "${length(split(",", var.azs))}" # Comment out count to only have 1 NAT
 
   lifecycle { create_before_destroy = true }
 }
 
 resource "aws_nat_gateway" "nat" {
-  # count         = "${length(split(",", var.public_subnet_ids))}" # Comment out count to only have 1 NAT
   allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
   subnet_id     = "${element(split(",", var.public_subnet_ids), count.index)}"
+
+  #count = "${length(split(",", var.azs))}" # Comment out count to only have 1 NAT
 
   lifecycle { create_before_destroy = true }
 }
 
-output "gateway_ids" { value = "${join(",", aws_nat_gateway.nat.*.id)}" }
+output "nat_gateway_ids" { value = "${join(",", aws_nat_gateway.nat.*.id)}" }
