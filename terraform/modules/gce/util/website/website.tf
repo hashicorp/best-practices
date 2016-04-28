98daf405f5d7a27b1779dc65b2c8a8bb2bc7edf5
Tim <tim@hashicorp.com>
2016-04-28 13:46:53 -0700

    Updates to the website buckets

--- terraform/modules/gce/util/website/website.tf
+++ terraform/modules/gce/util/website/website.tf
@@ -9,19 +9,19 @@ variable "index_page"    { default = "index.html" }
 variable "error_page"    { default = "error.html" }
 
 resource "google_storage_bucket" "website" {
-  bucket = "${var.fqdn}"
-  predefined_acl = "publicRead"
-  force_destroy = true
+  name            = "website"
+  predefined_acl  = "publicRead"
+  force_destroy   = true
 
   website {
-    main_page_suffix = "${var.index_page}"
-    not_found_page = "${var.error_page}"
+    main_page_suffix  = "${var.index_page}"
+    not_found_page    = "${var.error_page}"
   }
 }
 
 resource "google_storage_bucket_object" "index" {
+  name    = "index"
   bucket  = "${google_storage_bucket.website.self_link}"
-  key     = "${var.index_page}"
   content =  <<EOF
 You've reached the ${var.fqdn} index page
 EOF
@@ -30,8 +30,8 @@ EOF
 }
 
 resource "google_storage_bucket_object" "error" {
+  name    = "error"
   bucket  = "${google_storage_bucket.website.self_link}"
-  key     = "${var.error_page}"
   content =  <<EOF
 You've reached the ${var.fqdn} error page
 EOF
@@ -40,11 +40,11 @@ EOF
 }
 
 resource "google_dns_record_set" "website" {
-  managed_zone = "${var.managed_zone}"
-  name    = "${var.fqdn}"
-  type    = "A"
-  ttl = 300
-  rrdata = ["${google_storage_bucket.website.self_link}"]
+  managed_zone  = "${var.managed_zone}"
+  name          = "${var.fqdn}"
+  type          = "A"
+  ttl           = 300
+  rrdatas       = ["${google_storage_bucket.website.self_link}"]
 }
 
 output "bucket"         { value = "${google_storage_bucket.website.bucket}" }
